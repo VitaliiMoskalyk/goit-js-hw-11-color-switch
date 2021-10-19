e398920fe939a5895ce9b8ff01cb4b7338d18a71
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2021-10-19 22:05:49 +0300

    +

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import './sass/main.scss';
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const startBtnRef = document.querySelector('[data-action="start"]');
+startBtnRef.addEventListener('click', changerColor);
+
+const stopBtnRef = document.querySelector('[data-action="stop"]');
+stopBtnRef.addEventListener('click', changerColor );
+let intervalId = null;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function changerColor(event) {
+    
+    if (event.target.dataset.action === 'start') {
+        intervalId=setInterval(()=>event.target.closest('body').style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)], 1000);
+        startBtnRef.removeEventListener('click',changerColor);
+    };
+
+    if (event.target.dataset.action === 'stop') {
+        clearInterval(intervalId);
+        startBtnRef.addEventListener('click', changerColor);
+        
+    }
+    
+}
+
